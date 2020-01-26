0da2ef2a878dc5cd8feb9ca3f787b5e5dc37de9f
Shunichi Kanade <kanade.shunichi@gmail.com>
2020-01-26 13:25:10 +0900

    feature to create, update, search people and bootstrap

--- src/main/java/com/example/shun/app/PeopleController.java
+++ src/main/java/com/example/shun/app/PeopleController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path="/")
@@ -20,12 +20,79 @@ public class PeopleController {
     @Autowired
     private PeopleRepository peopleRepository;
 
+    private void setAppName(Model model) {
+        model.addAttribute("appName", appName);
+    }
 
     @GetMapping(path="/")
     public String getAllPeopleView(Model model) {
-        model.addAttribute("appName", appName);
+        setAppName(model);
         model.addAttribute("peoples", findAll());
         return "people"; // view
+    }
+
+    @GetMapping(path="/people/create")
+    public String create(Model model) {
+        setAppName(model);
+        return "people_create";
+    }
+
+    @PostMapping(path="/people/save")
+    public String save(@RequestParam("first") String first, @RequestParam("last") String last, Model model) {
+        setAppName(model);
+        People people = new People(first, last);
+        peopleRepository.save(people);
+        model.addAttribute("people", people);
+        return "people_update";
+    }
+
+    @GetMapping(path="/people/update")
+    public String updateView(@RequestParam("id") Long id, Model model) {
+        setAppName(model);
+        People people = findPeopleById(id);
+        model.addAttribute("people", people);
+        return "people_update";
+    }
+
+    private People findPeopleById(Long id) {
+        Optional<People> peopleOptional = peopleRepository.findById(id);
+        if (!peopleOptional.isPresent()) {
+            throw new RuntimeException("Person with id: "+id+" does NOT exist");
+        }
+        People people = peopleOptional.get();
+        return people;
+    }
+
+    @PostMapping(path="/people/update")
+    public String update(@RequestParam("id") Long id, @RequestParam("first") String first, @RequestParam("last") String last,
+                         Model model) {
+        setAppName(model);
+        People people = findPeopleById(id);
+        people.setFirst(first);
+        people.setLast(last);
+
+        peopleRepository.save(people);
+        model.addAttribute("people", people);
+        return "people_update";
+    }
+
+    @GetMapping(path="/people/delete")
+    public String delete(@RequestParam("id") Long id, Model model) {
+        setAppName(model);
+        peopleRepository.deleteById(id);
+        return getAllPeopleView(model);
+    }
+
+    @GetMapping(path="/people/search")
+    public String searchView(Model model) {
+        setAppName(model);
+        return "people_search";
+    }
+
+    @RequestMapping(value="/people/search", produces="application/json", consumes="application/json")
+    @ResponseBody
+    public List<People> search(Model model) {
+        return findAll();
     }
 
     private List<People> findAll() {
--- src/main/java/com/example/shun/app/model/People.java
+++ src/main/java/com/example/shun/app/model/People.java
@@ -1,6 +1,7 @@
 package com.example.shun.app.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,11 +10,17 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class People {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public People(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
 
     private String first;
     private String last;
